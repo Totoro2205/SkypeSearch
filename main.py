3dde9b565d0b7ddc64c4fa1e57590138b9be830b
Beam <6260045+8C@users.noreply.github.com>
2022-04-27 10:29:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 import asyncio
 import sys
 import os
-from pathlib import Path
 
 from modules.search import search
 
 if __name__ == "__main__":
-    modules = ["search"]
 
-    if len(sys.argv) <= 1 or sys.argv[1].lower() not in modules:
-        print("Please choose a module.\n")
-        print("Available modules :")
-        for module in modules:
-            print(f"- {module}")
-        exit()
+    if len(sys.argv) < 2:
+        data = None
+    else:
+        data = ' '.join(sys.argv[1:])
 
-    with open('token.txt', 'r', encoding='utf-8') as f:
+    with open('token.txt', 'r') as f:
         token = f.read().strip()
 
-    module = sys.argv[1].lower()
-    if len(sys.argv) >= 3:
-        data = sys.argv[2]
-    else:
-        data = None
-
     loop = asyncio.get_event_loop()
-    if module == "search":
-        loop.run_until_complete(search(data, token))
+    loop.run_until_complete(search(data, token))
