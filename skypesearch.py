7ab187ac5d036cadf4910697895256593d2d2a2e
Beam <6260045+8C@users.noreply.github.com>
2022-04-28 11:05:53 +0800

    Update skypesearch.py

--- skypesearch.py
+++ skypesearch.py
@@ -7,12 +7,16 @@ from modules.search import search
 if __name__ == "__main__":
 
     if len(sys.argv) < 2:
-        data = None
-    else:
-        data = ' '.join(sys.argv[1:])
+        print('Please enter valid text to search in Skype.')
+        exit()
+
+    data = ' '.join(sys.argv[1:])
 
     with open('token.txt', 'r') as f:
         token = f.read().strip()
+        if token == '':
+            print('Please insert your Skype access token in token.txt and re-run the program.')
+            exit()
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(search(data, token))
